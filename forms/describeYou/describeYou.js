e320d19c45a8fedf344902f64bdd0f5178d0dff3
kks76369 <60583217+kks76369@users.noreply.github.com>
2020-03-28 09:53:58 -0600

    commit2

--- forms/describeYou/describeYou.js
+++ forms/describeYou/describeYou.js
@@ -1,5 +1,8 @@
 
+rdoVerbs.onclick=function(){
+  lblVerb.value = (`I would agree that you are a ${$("input[name=rdoVerbs]:checked").prop("value")} person too! `)
+}
 
-
-
-
+btnfavExercises.onclick=function(){
+  ChangeForm(ABfavExercises)
+}
