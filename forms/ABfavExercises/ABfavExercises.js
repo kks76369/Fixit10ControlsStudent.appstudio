334aa5ba956cff9ce5a121a34fd924c4abbaeb83
kks76369 <60583217+kks76369@users.noreply.github.com>
2020-04-05 16:19:42 -0600

    commit3
    commit3

--- forms/ABfavExercises/ABfavExercises.js
+++ forms/ABfavExercises/ABfavExercises.js
@@ -1,11 +1,36 @@
-/*var exercises = ["pullups","pushups","situps","jogging","plank","curls"]
+var exercises = ["pullups","pushups","situps","jogging","plank","curls"]
 var coreExercises = ["situps","plank"]
+let response = ' '
+let favExercises = ' '
 
 favExercises.onshow=function(){
-
+  selExercises.clear()
+  for (i = 0; i <= exercises.length - 1; i++) {
+       selExercises.addItem(exercises[i])
+       }
 }
 
 selExercises.onfocusout=function(){
-
+ let exercisesChoice = (selExercises)
+     let stringChoice = exercisesChoice.toString()
+     let splitString = stringChoice.split(",")
+     let joinString = splitString.join(" and ")
+     favExercises = joinString
 }
-*/
+
+submitExercise.onclick=function(){
+      lblOutput.toggle()
+     if (favExercises == "situps and plank") {
+          lblOutput.toggle()
+          lblOutput.value = "You chose " + favExercises
+     } else {
+          lblOutput.toggle()
+          lblOutput.value = "You did not choose the correct exercises."
+          }
+}
+
+btnMobileNav.onclick=function(){
+  ChangeForm(mobileNav)
+}
+
+
